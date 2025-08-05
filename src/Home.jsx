7374547c61a2bf78d9807814338ad0787daca9a7
noorjahan220 <noorjahanmeem220@gmail.com>
2025-08-05 21:14:37 +0600

    add feedback section

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import About from './About';
+import TestimonialsCarousel from './feedback';
+import Feedback from './feedback';
 
 const Home = () => {
     return (
         <div>
             
             <About/>
+            <Feedback/>
         </div>
     );
 };
